73492185b069db8082c8e38fd6c79977b73b479f
SevLG <sebastian.gunnari.lindbom@scilifelab.se>
2024-03-26 18:55:00 +0100

    Testing building flask APIs following course by Teclado

---
+++ app.py
@@ -0,0 +1,75 @@
+import uuid
+from flask import Flask, request
+from flask_smorest import abort
+from db import items, stores
+
+app = Flask(__name__)
+
+
+
+@app.get("/store")
+def get_stores():
+    return {"stores": list(stores.values())}
+
+@app.post("/store")
+def create_store():
+    store_data = request.get_json()
+    if (
+        "name" not in store_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'name' is included in the JSON payload."
+        )
+    for store in store.values():
+        if  store_data["name"] == store["name"]:
+            abort(400, message=f"Item already exists.")
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
+
+@app.get("/item")
+def get_items():
+    return {"items": list(items.values())}
+
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload."
+        )
+    for item in item.values():
+        if  (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"Item already exists.")
+    if item_data["store_id"] not in stores:
+        abort(404,message="Store not found.")
+    
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+
+    return item, 201
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id], 200
+    except:
+        abort(404,message="Store not found.")
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id], 200
+    except:
+        abort(404,message="Item not found.")
